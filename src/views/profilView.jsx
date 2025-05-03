2fd09671810931682d367a2fc83cc5edaa928218
Lenwr <saidabou.lateam@gmail.com>
2025-05-03 12:51:55 +0200

    profileCard

--- src/views/profilView.jsx
+++ src/views/profilView.jsx
@@ -20,7 +20,7 @@ const Profil = () => {
         {/* Buttons */}
         <div className="flex justify-center gap-2 mt-4">
         <a href="tel:0781701384" className="inline-block">
-  <button className="px-4 py-1 bg-gray-100 rounded-full text-sm">
+  <button className="px-4 py-1 bg-gray-100 animate-bounce rounded-full text-sm">
     Appeler
   </button>
 </a>
@@ -37,16 +37,16 @@ const Profil = () => {
         </div>
 
         {/* Description */}
-        <p className="mt-4 text-sm text-gray-700">
+        <p className="my-4 text-sm text-gray-700">
         Vente de fenêtres et portes-fenêtres en pvc avec possibilité de faire la pose 📥
         </p>
       </div>
 
       {/* Information Section */}
-      <div className="px-6 py-4 border-t text-sm text-gray-700">
+      <div className="px-4 pt-8 border-t text-sm text-gray-700">
         <h3 className="font-semibold text-base mb-3">Information</h3>
         <div className="mb-2">
-          <span className="font-medium">🌐 Envoi || transport || deménagement :</span>{" "}
+          <span className="font-medium">🌐 Transport et envoi à l'international :</span>{" "}
           <a href="https://www.wefretafrica.fr/" className="text-[#07c2e5]" target="_blank" rel="noreferrer">
             wefretafrica
           </a>
@@ -58,14 +58,14 @@ const Profil = () => {
           </a>
         </div>
         <div className="mb-2">
-          <span className="font-medium">📞 Phone:</span> 07 81 70 13 84
+          <span className="font-medium">📞 Téléphone:</span> 07 81 70 13 84
         </div>
         <div className="mb-2">
           <span className="font-medium"></span> 
         </div>
 
         {/* Tags */}
-        <h3 className="font-semibold text-base mb-3">Liste des dimensions</h3>
+        <h3 className="font-semibold text-base my-4">Liste des dimensions</h3>
         <div className="flex justify-around flex-wrap gap-4 mt-4">
           {["140L/164H","212L/164H","100L/215H","130L/215H","300L/140H","190L/205H","90L/125H","183L/128H","186L/140H",
             "96L/92H","106L/162H","85L/200H","46L/90H","115L/112H","120L/120H"
